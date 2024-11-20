9e07ee26f9149d4d6d0424a3e745a4478951edec
zzoo0123 <0123suh@gmail.com>
2024-11-20 10:35:14 +0900

    feat: CancellationAdmin Dto

--- src/main/java/com/plantify/refund/domain/dto/request/CancellationStatusRequest.java
+++ src/main/java/com/plantify/refund/domain/dto/request/CancellationStatusRequest.java
@@ -2,5 +2,7 @@ package com.plantify.refund.domain.dto.request;
 
 import com.plantify.refund.domain.entity.Status;
 
-public record CancellationUpdateRequest(Status status) {
-}
+public record CancellationStatusRequest(
+        Status status,
+        String reason
+) {}
--- src/main/java/com/plantify/refund/domain/dto/response/CancellationAdminResponse.java
+++ src/main/java/com/plantify/refund/domain/dto/response/CancellationAdminResponse.java
@@ -1,25 +1,26 @@
 package com.plantify.refund.domain.dto.response;
 
 import com.plantify.refund.domain.entity.Cancellation;
+import com.plantify.refund.domain.entity.Status;
 
 import java.time.LocalDateTime;
 
-public record CancellationResponse(
+public record CancellationAdminResponse(
         Long cancellationId,
-        Long paymentId,
         Long userId,
+        Long paymentId,
         String reason,
-        String status,
+        Status status,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
-    public static CancellationResponse from(Cancellation cancellation) {
-        return new CancellationResponse(
+    public static CancellationAdminResponse from(Cancellation cancellation) {
+        return new CancellationAdminResponse(
                 cancellation.getCancellationId(),
-                cancellation.getPaymentId(),
                 cancellation.getUserId(),
+                cancellation.getPaymentId(),
                 cancellation.getReason(),
-                cancellation.getStatus().name(),
+                cancellation.getStatus(),
                 cancellation.getCreatedAt(),
                 cancellation.getUpdatedAt()
         );
