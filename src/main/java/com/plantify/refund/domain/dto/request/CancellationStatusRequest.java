31d745fb18771793c28625d8d5e91a43a4abcd2a
zzoo0123 <0123suh@gmail.com>
2024-11-20 11:33:12 +0900

    fix: merge error

--- src/main/java/com/plantify/refund/domain/dto/request/CancellationStatusRequest.java
+++ src/main/java/com/plantify/refund/domain/dto/request/CancellationStatusRequest.java
@@ -2,14 +2,7 @@ package com.plantify.refund.domain.dto.request;
 
 import com.plantify.refund.domain.entity.Status;
 
-<<<<<<<< HEAD:src/main/java/com/plantify/refund/domain/dto/request/CancellationStatusRequest.java
 public record CancellationStatusRequest(
         Status status,
         String reason
 ) {}
-========
-public record RefundStatusRequest(
-        Status status,
-        String reason
-) {}
->>>>>>>> feat/refund-service:src/main/java/com/plantify/refund/domain/dto/request/RefundStatusRequest.java
--- src/main/java/com/plantify/refund/domain/dto/request/RefundUserRequest.java
+++ src/main/java/com/plantify/refund/domain/dto/request/RefundUserRequest.java
@@ -3,23 +3,13 @@ package com.plantify.refund.domain.dto.request;
 import com.plantify.refund.domain.entity.Refund;
 import com.plantify.refund.domain.entity.Status;
 
-<<<<<<<< HEAD:src/main/java/com/plantify/refund/domain/dto/request/CancellationUserRequest.java
-public record CancellationUserRequest(
-========
 public record RefundUserRequest(
->>>>>>>> feat/refund-service:src/main/java/com/plantify/refund/domain/dto/request/RefundUserRequest.java
         Long paymentId,
         String reason,
         String refundTo
 ) {
-<<<<<<<< HEAD:src/main/java/com/plantify/refund/domain/dto/request/CancellationUserRequest.java
-    public Cancellation toEntity(Long userId) {
-        return Cancellation.builder()
-========
-
     public Refund toEntity(Long userId) {
         return Refund.builder()
->>>>>>>> feat/refund-service:src/main/java/com/plantify/refund/domain/dto/request/RefundUserRequest.java
                 .userId(userId)
                 .paymentId(paymentId)
                 .reason(reason)
